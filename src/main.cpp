d4d61bfe57eff6c1fd6639bd1c7bef88923a2de7
Akash Vibhute <akash@openrobotics.org>
2021-04-20 16:02:21 +0800

    Adding a working example of OTA updater AP and Station modes

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+
+const char* ssid = "SSID_HERE";
+const char* password = "PSK_HERE";
+
+AsyncWebServer server(80);
+
+void setup(void) {
+  Serial.begin(115200);
+  
+  /*
+  // Start in Station mode
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  */
+
+  // Start in AP mode
+  Serial.print("Setting AP");
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP32.");
+  });
+
+  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  AsyncElegantOTA.loop();
+}
